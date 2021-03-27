952639a7d05d83ff06928c5c73119a1bfb19cb4f
Michaela Marková <Michaela@michaela-air.home>
2021-03-27 22:13:16 +0000

    add-javascript

--- script.js
+++ script.js
@@ -1,62 +1,133 @@
-let carts = document.querySelectorAll('.add-to-cart')
+let carts = document.querySelectorAll(".add-to-cart");
 
 let products = [
   {
-    name: 'Monstera'
-    tag: 'monstera'
-    price: 25
-    inCart: 0
-  }, 
+    name: "Monstera",
+    tag: "monstera",
+    price: 25,
+    inCart: 0,
+  },
   {
-    name: 'Ivy'
-    tag: 'ivy'
-    price: 25
-    inCart: 0
-  }, 
+    name: "Ivy",
+    tag: "ivy",
+    price: 15,
+    inCart: 0,
+  },
   {
-    name: 'Fig'
-    tag: 'fig'
-    price: 25
-    inCart: 0
-  }, 
+    name: "Fig",
+    tag: "fig",
+    price: 30,
+    inCart: 0,
+  },
   {
-    name: 'Fern'
-    tag: 'fern'
-    price: 25
-    inCart: 0
-  }, 
-]
+    name: "Fern",
+    tag: "fern",
+    price: 15,
+    inCart: 0,
+  },
+];
 
-for (let i=0; i <carts.length; i++) {
-  carts[i].addEventListener('click', () => 
-  {cartNumbers("added to cart");
-})
+for (let i = 0; i < carts.length; i++) {
+  carts[i].addEventListener("click", () => {
+    cartNumbers(products[i]);
+    totalCost(products[i]);
+  });
 }
 
-function onLoadCartNumbers(){
-  let productNumbers = localStorage.getItem('cartNumbers');
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
 
   if (productNumbers) {
-    document.querySelector('.cart span').textContent = productNumbers;
+    document.querySelector(".cart span").textContent = productNumbers;
   }
-
 }
 
-function cartNumbers(){
-  let productNumbers = localStorage.getItem('cartNumbers');
+function cartNumbers(product) {
+  let productNumbers = localStorage.getItem("cartNumbers");
 
   productNumbers = parseInt(productNumbers);
 
-  if (productNumbers){
-    localStorage.setItem('cartNumbers', productNumbers +1);
-    document.querySelector('cart span').textContent = productNumbers +1;
-  } 
-  else {
-    localStorage.setItem('cartNumbers',1);
-    document.querySelector('cart span').textContent = 1;
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".cart span").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".cart span").textContent = 1;
+  }
+
+  setItems(product);
+}
+
+function setItems(product) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product,
+    };
+  }
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+function totalCost(product) {
+  let cartCost = localStorage.getItem("totalCost");
+
+  console.log("My cartCost is", cartCost);
+  console.log(typeof cartCost);
+
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem("totalCost", cartCost + product.price);
+  } else {
+    localStorage.setItem("totalCost", product.price);
   }
 }
+/*-----------Cart page----------*/
+function displayCart() {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let productContainer = document.querySelector(".products");
+  let cartCost = localStorage.getItem("totalCost");
+
+  if (cartItems && productContainer) {
+    productContainer.innerHTML = "";
+    Object.values(cartItems).map((item) => {
+      productContainer.innerHTML += `
+      <div class="product">
+      <img src= "./images/${item.tag}.jpg">
+      <span>${item.name}</span>
+      </div>
+      <div class="price">£${item.price},00</div>
+      <div class="quantity">
+      <span>${item.inCart}</span>
+      </div>
+      <div class="total">
+      £${item.inCart * item.price},00
+      </div>
+      `;
+    });
+    productContainer.innerHTML += `
+    <div class="basketTotalContainer">
+    <h4 class="basketTotalTitle">
+    Basket total
+    </h4>
+    <h4 class="basketTotal">
+    £${cartCost},00
+    </h4>
+
+
+    </div>`;
+  }
 }
-
 onLoadCartNumbers();
-
+displayCart();
